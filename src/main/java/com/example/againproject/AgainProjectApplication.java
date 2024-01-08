8c298be869b7228abe56b259e71e243ec322f4cf
admin <tmdco12@naver.com>
2024-01-08 09:07:27 +0900

    Initial commit

---
+++ src/main/java/com/example/againproject/AgainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.againproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgainProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/againproject/AgainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.againproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
