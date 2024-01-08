3ee7cd4c035e5788357e023e8ed11bec87bab1cb
admin <tmdco12@naver.com>
2024-01-08 09:41:14 +0900

    Controller - Model 추가

--- src/main/java/com/example/againproject/controller/FirstController.java
+++ src/main/java/com/example/againproject/controller/FirstController.java
@@ -1,13 +1,14 @@
 package com.example.againproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi") // 접속할 URL 주소
-    public String hi() {
+    public String hi(Model model) {
         return "greetings"; // templats/greetings.mustache -> 브라우저로 전송!
     }
 }
