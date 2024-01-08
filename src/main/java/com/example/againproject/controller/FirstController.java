87b0ebb4099c866617606526dde079dd6614e472
admin <tmdco12@naver.com>
2024-01-08 09:38:31 +0900

    Controller 작성

---
+++ src/main/java/com/example/againproject/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.againproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi") // 접속할 URL 주소
+    public String hi() {
+        return "greetings"; // templats/greetings.mustache -> 브라우저로 전송!
+    }
+}
