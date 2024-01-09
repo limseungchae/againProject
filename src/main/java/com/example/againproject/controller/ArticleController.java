766799eb6d8ba2066a1bd787602d1a8e361d6275
admin <tmdco12@naver.com>
2024-01-09 14:23:29 +0900

    처리 흐름 개요

--- src/main/java/com/example/againproject/controller/ArticleController.java
+++ src/main/java/com/example/againproject/controller/ArticleController.java
@@ -17,6 +17,12 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        // 1. Dto를 변환! Entity!
+        Article article = form.toEntity();
+
+        // 2. Repository에게 Entity를 DB안에 저장하게 함!
+
         return "";
     }
 }
