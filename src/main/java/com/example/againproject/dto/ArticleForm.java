63c26d08ed22d5a49ec247a10604b05e0d34ae17
admin <tmdco12@naver.com>
2024-01-09 13:50:49 +0900

    DTO 작성하기

---
+++ src/main/java/com/example/againproject/dto/ArticleForm.java
@@ -0,0 +1,20 @@
+package com.example.againproject.dto;
+
+public class ArticleForm {
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // 데이터가 잘받아졌는지 확인하기 위해
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
