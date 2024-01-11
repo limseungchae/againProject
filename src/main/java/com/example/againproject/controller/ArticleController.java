b4e0a676670b58ac804e55781fa12d8c64930dc6
admin <tmdco12@naver.com>
2024-01-11 09:48:10 +0900

    데이터 조회해 출력하기 모델에 데이터 등록하기

--- src/main/java/com/example/againproject/controller/ArticleController.java
+++ src/main/java/com/example/againproject/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.againproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,11 +44,12 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
-    public String show(@PathVariable Long id) { // 매개변수로 id 받아 오기
+    public String show(@PathVariable Long id, Model model) { // 매개변수로 id 받아 오기
         // 1. id를 조회해 데이터 가져오기 // 위에꺼 사용해도 이상없음
 //        Optional<Article> article = articleRepository.findById(id);
         Article articleEntity = articleRepository.findById(id).orElse(null);
         // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "";
     }
