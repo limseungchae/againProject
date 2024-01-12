674031898d9270b3fc58519e88416ed519b8e737
admin <tmdco12@naver.com>
2024-01-12 17:13:52 +0900

    DB에 저장하고 결과 페이지로 리다이렉트하기 엔티티를 DB에 저장하기

--- src/main/java/com/example/againproject/controller/ArticleController.java
+++ src/main/java/com/example/againproject/controller/ArticleController.java
@@ -85,6 +85,14 @@ public class ArticleController {
         Article articleEntity = form.toEntity();
         log.info(articleEntity.toString());
         // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        // 2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            articleRepository.save(articleEntity); // 엔티티를 DV에 저장(갱신)
+        }
+
         // 3. 수정 결과 페이지로 리다이렉트 하기
         return "";
     }
