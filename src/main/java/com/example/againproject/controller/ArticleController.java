51aa7bff5f6369b663e45941f684e35241a3b49e
admin <tmdco12@naver.com>
2024-01-13 23:44:04 +0900

    데이터 삭제하기 삭제할 대상 가져오기

--- src/main/java/com/example/againproject/controller/ArticleController.java
+++ src/main/java/com/example/againproject/controller/ArticleController.java
@@ -98,9 +98,11 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{id}/delete")
-    public String delete() {
+    public String delete(@PathVariable Long id) {
         log.info("삭제 요청이 들어왔습니다!!");
         // 1. 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
         // 2. 대상 엔티티 삭제하기
         // 3. 결과 페이지로 리다이렉트하기
         return "null";
