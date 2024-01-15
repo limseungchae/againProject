75dfc920b1e09d17ed438d71889db059fc447316
admin <tmdco12@naver.com>
2024-01-15 21:40:18 +0900

    REST API 구현하기 GET,POST,PATCH,DELETE

---
+++ src/main/java/com/example/againproject/api/FirstAPiController.java
@@ -0,0 +1,11 @@
+package com.example.againproject.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // REST API용 컨트롤러
+public class FirstAPiController {
+    // GET
+    // POST
+    // PATCH
+    // DELETE
+}
