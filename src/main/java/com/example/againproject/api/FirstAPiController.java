f8189f43d9025b793b0284a7c133ac963ed6e4ef
admin <tmdco12@naver.com>
2024-01-15 21:42:40 +0900

    REST API 구현하기 모든 게시글 조회하기

--- src/main/java/com/example/againproject/api/FirstAPiController.java
+++ src/main/java/com/example/againproject/api/FirstAPiController.java
@@ -1,10 +1,22 @@
 package com.example.againproject.api;
 
+import com.example.againproject.entity.Article;
+import com.example.againproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // REST API용 컨트롤러
 public class FirstAPiController {
+    @Autowired
+    private ArticleRepository articleRepository;
     // GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
     // POST
     // PATCH
     // DELETE
