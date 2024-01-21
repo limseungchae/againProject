dafab8dcc93ba3a43a9f4b86aa17e8a019ac23b2
admin <tmdco12@naver.com>
2024-01-21 12:15:47 +0900

    index() 테스트하기 실제 데이터

--- src/test/java/com/example/againproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/againproject/service/ArticleServiceTest.java
@@ -1,8 +1,11 @@
 package com.example.againproject.service;
 
+import com.example.againproject.entity.Article;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +19,7 @@ class ArticleServiceTest {
     void index() {
         // 1. 예상 테이터
         // 2. 실제 데이터
+        List<Article> articles = articleService.index();
         // 3. 비교 및 검증
     }
 }
