3d2db828c84cc34e6d04cef3eceee4482c88aeb0
admin <tmdco12@naver.com>
2024-01-23 11:43:53 +0900

    댓글 리파지터리 테스트 코드 작성하기 findByArticleId 주석 작성

--- src/test/java/com/example/againproject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/againproject/repository/CommentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.againproject.repository;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,7 +14,15 @@ class CommentRepositoryTest {
     CommentRepository commentRepository; // commentRepository 객체 주입
 
     @Test
+    @DisplayName("특정 게시글의 모든 댓글 조회")
     void findByArticleId() {
+        /* Case 1: 4번 게시글의 모든 댓글 조회 */
+        {
+            // 1. 입력 데이터 준비
+            // 2. 실제 데이터
+            // 3. 예상 데이터
+            // 4. 비교 및 검증
+        }
     }
 
     @Test
