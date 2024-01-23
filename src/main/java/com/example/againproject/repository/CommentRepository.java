1796b43749d5563144f87b392d9513fd6a44afc7
admin <tmdco12@naver.com>
2024-01-23 09:47:36 +0900

    댓글 리파지터리 만들기

---
+++ src/main/java/com/example/againproject/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.example.againproject.repository;
+
+import com.example.againproject.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
