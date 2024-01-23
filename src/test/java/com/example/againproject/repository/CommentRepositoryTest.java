132a0158d77a70b989de22731d5d202d23ee9d66
admin <tmdco12@naver.com>
2024-01-23 12:19:38 +0900

    댓글 리파지터리 테스트 코드 작성하기 findByArticleId 예상 데이터

--- src/test/java/com/example/againproject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/againproject/repository/CommentRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.example.againproject.repository;
 
+import com.example.againproject.entity.Article;
 import com.example.againproject.entity.Comment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +28,11 @@ class CommentRepositoryTest {
             // 2. 실제 데이터
             List<Comment> comments = commentRepository.findByArticleId(articleId);
             // 3. 예상 데이터
+            Article article = new Article(4L, "당신의 인생 영화는?", "댓글 고");
+            Comment a = new Comment(1L, article, "Park", "굿 윌 헌팅");
+            Comment b = new Comment(2L, article, "Kim", "아이 엠 샘");
+            Comment c = new Comment(3L, article, "Choi", "쇼생크 탈출");
+            List<Comment> expected = Arrays.asList(a, b, c);
             // 4. 비교 및 검증
             assertEquals(expected.toString(), comments.toString(), "4번 글의 모든 댓글을 출력!");
         }
