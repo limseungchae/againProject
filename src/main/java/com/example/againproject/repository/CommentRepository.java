b73988670f33c80963b5a84c4852c881cdf9a808
admin <tmdco12@naver.com>
2024-01-23 09:47:52 +0900

    댓글 리파지터리 만들기 주석 작성

--- src/main/java/com/example/againproject/repository/CommentRepository.java
+++ src/main/java/com/example/againproject/repository/CommentRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // 특정 게시글의 모든 댓글 조회
+    // 특정 닉네임의 모든 댓글 조회
 }
