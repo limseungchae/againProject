3de698faac7d822da764bc8c55988562461352ec
admin <tmdco12@naver.com>
2024-01-24 13:39:34 +0900

    요청 처리할 서비스 만들기 엔티티 -> DTO 변환

--- src/main/java/com/example/againproject/service/CommentService.java
+++ src/main/java/com/example/againproject/service/CommentService.java
@@ -7,7 +7,7 @@ import com.example.againproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +21,12 @@ public class CommentService {
         // 1. 댓글 조회
         List<Comment> comments = commentRepository.findByArticleId(articleId);
         // 2. 엔티티 -> DTO 변환
+        ArrayList<CommentDto> dtos = new ArrayList<>();
+        for (int i = 0; i < comments.size(); i++) { // 조회한 댓글 엔티티 수만큼 반복하기
+            Comment c = comments.get(i); // 조회한 댓글 엔티티 하나씩 가져오기
+            CommentDto dto = CommentDto.createCommentDto(c); // 엔티티를 DTO로 변환
+            dtos.add(dto); // 변환한 DTO를 dtos 리스트에 삽입
+        }
         // 3. 결과 반환
     }
 }
