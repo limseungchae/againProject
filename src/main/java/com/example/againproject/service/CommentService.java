0fad80fb6104a9595160af71fe56c1071f176255
admin <tmdco12@naver.com>
2024-01-24 12:58:34 +0900

    요청 처리할 서비스 만들기

--- src/main/java/com/example/againproject/service/CommentService.java
+++ src/main/java/com/example/againproject/service/CommentService.java
@@ -1,9 +1,12 @@
 package com.example.againproject.service;
 
+import com.example.againproject.dto.CommentDto;
 import com.example.againproject.repository.ArticleRepository;
 import com.example.againproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -11,4 +14,10 @@ public class CommentService {
     private CommentRepository commentRepository; // 댓글 리파지터리
     @Autowired
     private ArticleRepository articleRepository; // 게시글 리파지터리
+
+    public List<CommentDto> comments(Long articleId) {
+        // 1. 댓글 조회
+        // 2. 엔티티 -> DTO 변환
+        // 3. 결과 반환
+    }
 }
