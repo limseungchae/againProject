6bd7b65303079aeacd80de598818f1a72c826155
admin <tmdco12@naver.com>
2024-01-25 09:56:17 +0900

    요청을 처리할 서비스 만들기 댓글 수정

--- src/main/java/com/example/againproject/service/CommentService.java
+++ src/main/java/com/example/againproject/service/CommentService.java
@@ -57,6 +57,7 @@ public class CommentService {
                 .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패!" +
                         "대상 댓글이 없습니다."));// 없으면 에레 메시지 출력
         // 2. 댓글 수정
+        target.patch(dto);
         // 3. DB로 갱신
         // 4. 댓글 엔티티를 DTO로 변환 및 반환
         return dto;
