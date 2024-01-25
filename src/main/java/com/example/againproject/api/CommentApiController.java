90cea275b207eae1d7106e8a16ef605df5e2d2da
admin <tmdco12@naver.com>
2024-01-25 10:29:26 +0900

    댓글 삭제 컨트롤러 만들기

--- src/main/java/com/example/againproject/api/CommentApiController.java
+++ src/main/java/com/example/againproject/api/CommentApiController.java
@@ -40,4 +40,11 @@ public class CommentApiController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
     // 4. 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        // 서비스에 위임
+        CommentDto deleteDto = commentService.delete(id);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(deleteDto);
+    }
 }
